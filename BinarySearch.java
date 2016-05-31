078d5856c9d3fd1b1d03570baad47421325b9a6c
Conor Ford <conorford1991@gmail.com>
2016-05-31 15:31:25 +0100

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,4 @@
-// Binary search is O(log n) as it halfs the elements to be searched each interation. 
+// Binary search is O(log n) as it half the elements to be searched each interation. 
 import java.util.*;
 
 class BinarySearch{
